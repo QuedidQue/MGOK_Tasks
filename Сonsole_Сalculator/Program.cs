0a3b622d22e54366fe9b293243d655ead5b4dd2a
Taldykin Denis <118611368+QuedidQue@users.noreply.github.com>
2022-11-29 20:26:04 +0300

    Add files via upload
    Сonsole_Сalculator

---
+++ Сonsole_Сalculator/Program.cs
@@ -0,0 +1,82 @@
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Консольный калькулятор C#");
+    double firstValue, secondValue;
+    string action;
+
+
+    try
+    {
+        Console.WriteLine("Введи 1 число");
+        firstValue = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введи операцию + - * / ");
+        action = Console.ReadLine();
+        Console.WriteLine("Введи 2 число");
+        secondValue = double.Parse(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Введено неверное число или оператор");
+        Console.ReadLine();
+        continue;
+    }
+    /*
+    switch (action)
+    {
+        case "+":
+            Console.WriteLine("Ответ");
+            Console.WriteLine(firstValue + secondValue);
+            break;
+
+        case "-":
+            Console.WriteLine("Ответ");
+            Console.WriteLine(firstValue - secondValue);
+            break;
+
+        case "*":
+            Console.WriteLine("Ответ");
+            Console.WriteLine(firstValue * secondValue);
+            break;
+
+        case "/":
+            Console.WriteLine("Ответ");
+            if (secondValue == 0)                   
+                Console.WriteLine(0);                    
+            else           
+               Console.WriteLine(firstValue / secondValue);
+            break;
+        default:
+            Console.WriteLine("Введено неверное число или оператор");
+            break;
+    }  */
+
+    if (action == "+")
+    {
+        Console.WriteLine("Ответ");
+        Console.WriteLine(firstValue + secondValue);
+    }
+    else if (action == "-")
+    {
+        Console.WriteLine("Ответ");
+        Console.WriteLine(firstValue - secondValue);
+    }
+    else if (action == "*")
+    {
+        Console.WriteLine("Ответ");
+        Console.WriteLine(firstValue * secondValue);
+    }
+    else if (action == "/")
+    {
+        Console.WriteLine("Ответ");
+        if (secondValue == 0)
+            Console.WriteLine(0);
+        else
+            Console.WriteLine(firstValue / secondValue);
+    }
+    else
+    {
+        Console.WriteLine("Введено неверное число или оператор");
+    }
+    Console.ReadLine();
+}
